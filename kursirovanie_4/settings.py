aa2476a445e30bab6167b112521758d5d746bb33
Roma <romachne@gmail.com>
2022-07-06 06:25:34 +0500

    front connected

--- kursirovanie_4/settings.py
+++ kursirovanie_4/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for kursirovanie_4 project.
 
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'delivery.apps.DeliveryConfig',
     'rest_framework',
     'simple_history',
+    'core',
 ]
 
 REST_FRAMEWORK = {
@@ -64,7 +67,7 @@ ROOT_URLCONF = 'kursirovanie_4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'react-kurs_4', 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'react-kurs_4', 'build', 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- kursirovanie_4/urls.py
+++ kursirovanie_4/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from core.views import front
 
 
 urlpatterns = [
-    path('', include('delivery.urls')),
     path('admin/', admin.site.urls),
+    path("", front, name="front"),
 ]
